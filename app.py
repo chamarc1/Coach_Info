93a3b853b9c7a114bde2f23030cc3fc8dca73d30
chamarc1 <chamarc1@outlook.com>
2023-02-28 14:48:50 -0500

    login and registration completed

--- app.py
+++ app.py
@@ -6,10 +6,34 @@ Date: 02/20/2023
 Lab Week 6
 Purpose: Python program that allows a user to load Flask website regarding a Soccer Coach
 """
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, render_template, request, session, redirect, url_for
+import sqlite3, re
+from werkzeug.security import check_password_hash, generate_password_hash
 from datetime import datetime
 
+# initialize Flask app
 app = Flask(__name__)
+
+# set up session secret key
+app.secret_key = 'super secret key'
+
+# define database connection function
+def get_db():
+    conn = sqlite3.connect('users.db')
+
+    # create a cursor object to execute SQL commands
+    cursor = conn.cursor()
+
+    # create a table
+    cursor.execute("""CREATE TABLE IF NOT EXISTS users
+                (id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT NOT NULL UNIQUE,
+                password_hash TEXT NOT NULL);""")
+    
+    # commit changes to the database
+    conn.commit()
+
+    return conn
 
 def get_date():
     """
@@ -29,18 +53,114 @@ def index():
     """
     name = "Charlemagne Marc"
     date = get_date()
-    return render_template("index.html", name=name, date=date)
+    # check if user is logged in
+    if 'user_id' in session:
+        user_id = session['user_id']
+        return render_template('index.html', name=name, date=date, user_id=user_id)
+    # if user is not logged in, redirect to login page
+    return redirect('/login')
 
 
-@app.route('/home')
-def home():
-    """
-    home(): function that returns render template of home html
-    return: home.html: name of html file for home
-    """
+# login page route
+@app.route('/login', methods=['GET', 'POST'])
+def login():
     name = "Charlemagne Marc"
-    date = get_date()
-    return render_template("home.html", name=name, date=date)
+    # check if user is already logged in
+    if 'user_id' in session:
+        return redirect('/')
+    # if user is not logged in, check if form is submitted
+    if request.method == 'POST':
+        # get form data
+        username = request.form['username']
+        password = request.form['password']
+        # connect to database
+        conn = get_db()
+        cur = conn.cursor()
+        # execute query to get user with given username
+        cur.execute('SELECT * FROM users WHERE username = ?', (username,))
+        user = cur.fetchone()
+        # check if user exists and password is correct
+        if user is not None and check_password_hash(user[2], password):
+            # set user ID in session
+            session['user_id'] = user[0]
+            # redirect to home page
+            return redirect('/')
+        else:
+            # display error message
+            error = 'Invalid username or password'
+            return render_template('login.html', error=error, name=name)
+    # if form is not submitted, display login page
+    return render_template('login.html', name=name)
+
+
+def valid_password(password):
+    """
+    A password complexity should be enforced to include at least 12 characters in length, and
+    include at least 1 uppercase character, 1 lowercase character, 1 number and 1 special character.
+    """
+    # check for length
+    if len(password) < 12:
+        print("check length")
+        return False
+    # check for upper
+    if not re.search(r"[A-Z]", password):
+        print("no upper")
+        return False
+    # check for lower
+    if not re.search(r"[a-z]", password):
+        print("no lower")
+        return False
+    # check for digit
+    if not re.search("\d", password):
+        print("no digit")
+        return False
+    # check for special
+    if not re.search(r"[@_!#$%^&*()<>?/|}{~:]", password):
+        print("no special")
+        return False
+    # return True since passes all conditions
+    return True
+
+
+# registration page route
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    name = "Charlemagne Marc"
+    # check if user is already logged in
+    if 'user_id' in session:
+        return redirect('/')
+    # if user is not logged in, check if form is submitted
+    if request.method == 'POST':
+        # get form data
+        username = request.form['username']
+        password = request.form['password']
+        # if password is not valid error message and redirect to registration
+        if not valid_password(password):
+            # display error message
+            error = 'Invalid password.'
+            return render_template('registration.html', error=error, name=name)
+        # generate password hash
+        password_hash = generate_password_hash(password)
+        # connect to database
+        conn = get_db()
+        cur = conn.cursor()
+        # execute query to insert user into database
+        cur.execute('INSERT INTO users (username, password_hash) VALUES (?, ?)',
+                    (username, password_hash))
+        conn.commit()
+        # redirect to login page
+        return redirect('/login')
+    # if form is not submitted, display registration page
+    return render_template('registration.html', name=name)
+
+
+# logout route
+@app.route('/logout')
+def logout():
+    # remove user ID from session
+    session.pop('user_id', None)
+    # redirect to login page
+    return redirect('/login')
 
 
 @app.route('/about')
@@ -103,4 +223,4 @@ def admin():
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
